491df2e5a3c9f8a3c9555cbfadc20dd409d63341
Michael Carleton <micycle1@hotmail.co.uk>
2025-12-30 13:43:37 +0000

    cleanup predicates class

--- src/test/java/com/github/quickhull3d/ExactJavaPredicatesTest.java
+++ src/test/java/com/github/quickhull3d/ExactJavaPredicatesTest.java
@@ -1,6 +1,5 @@
 package com.github.quickhull3d;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
@@ -11,8 +10,6 @@ import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
 
 import org.junit.jupiter.api.Test;
-
-import com.github.quickhull3d.ExactJavaPredicates.SphereConfig;
 
 public class ExactJavaPredicatesTest {
 
